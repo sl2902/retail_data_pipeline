6182ead44b52f2d97af7bbd00d537767316df97c
sl2902 <laxmsun@hotmail.com>
2024-03-25 20:54:51 +0530

    Update the pubsublite reservation and topic names

--- dags/config_data/pubsublite_config.py
+++ dags/config_data/pubsublite_config.py
@@ -6,9 +6,9 @@ load_dotenv()
 project_number = os.getenv('project_id')
 cloud_region = os.getenv('region')
 zone_id = os.getenv('zone_id')
-reservation_id = "reservation_2"
+reservation_id = "reservation_3"
 throughput_capacity = 4
 num_partitions = 2
 regional = True
-txn_topic_id = "transaction_2"
-inv_topic_id = "inventory_2"
+txn_topic_id = "transaction_3"
+inv_topic_id = "inventory_3"
