d1766e070108da868bf7f824d36394a542157d50
sl2902 <laxmsun@hotmail.com>
2024-04-01 22:19:11 +0530

    Add line chart

--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -177,11 +177,13 @@ with st.sidebar:
    #  transforms(mv_txn_prod)
     year_list = sorted(mv_txn_prod["year"].unique())
     month_list = sorted(mv_txn_prod["month_name"].unique())
+    location_list = sorted(mv_txn_prod["location"].unique())
    #  day_list = sorted(mv_txn_prod["dow"].unique(), key=lambda x: dow[x])
     selected_year = st.selectbox('Select a year', year_list)
     selected_month = st.selectbox('Select a month', sorted(month_list, key=lambda x: mm_name[x]))
-   #  selected_day = st.selectbox('Select a day of week', list(dow.keys()))
+    # selected_day = st.selectbox('Select a day of week', list(dow.keys()))
     # selected_date = st.date_input("Select a date")
+    selected_location = st.selectbox('Select a location', location_list)
     color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
     selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
     mv_txn_prod["txn_date"] = pd.to_datetime(mv_txn_prod["txn_date"], format="%Y-%m-%d")
@@ -192,9 +194,9 @@ with st.sidebar:
     daily_pct_change = mv_txn_prod.groupby(["year", "month", "location"], as_index=False).agg({"quantity": 'sum'})
     daily_pct_change["prev_day_qty"] = daily_pct_change.groupby(["location"], as_index=False)["quantity"].shift(1)
     daily_pct_change["pct_change"] = (daily_pct_change["quantity"] - daily_pct_change["prev_day_qty"]) / daily_pct_change["prev_day_qty"]
-   #  daily_pct_change["year"] = daily_pct_change["txn_date"].dt.year
-   #  daily_pct_change["month"] = daily_pct_change["txn_date"].dt.month
-   #  daily_pct_change["dow"] = daily_pct_change["txn_date"].dt.day_name()
+    line_pct_change = mv_txn_prod.groupby(["txn_date", "location"], as_index=False).agg({'revenue': "sum"})
+    line_pct_change["prev_day_revenue"] = line_pct_change.groupby(["location"], as_index=False)["revenue"].shift(1)
+    line_pct_change["pct_change"] = (line_pct_change["revenue"] - line_pct_change["prev_day_revenue"]) / line_pct_change["prev_day_revenue"]
 
 
 def make_heatmap(input_df, input_x, input_y, input_z, input_color, input_color_theme):
@@ -251,6 +253,30 @@ def make_choropleth(input_df, grp_1, grp_2, input_id, input_column, input_color_
     )
     return choropleth
 
+# Line chart
+def make_linechart(input_df, input_x=None, input_y=None, input_color=None):
+    # https://stackoverflow.com/questions/53287928/tooltips-in-altair-line-charts
+    mm = mm_name[selected_month]
+    input_df["year"] = input_df["txn_date"].dt.year
+    input_df["month"] = input_df["txn_date"].dt.month
+    subset = input_df.query("(year == @selected_year) & (month == @mm) & (location == @selected_location)").reset_index(drop=True)
+    # subset = input_df.query("(txn_date == @selected_date)").reset_index(drop=True)
+    if len(subset) == 0:
+        st.write(f"No data for period {selected_year} and {selected_month}")
+        return
+    else:
+        subset["txn_date"] = subset["txn_date"].dt.date
+        subset["txn_date"] = subset["txn_date"].to_numpy().astype("datetime64[D]")
+        input_x, input_y = 'txn_date', 'pct_change'
+        linechart = alt.Chart(subset).mark_line().encode(
+            alt.X(f"yearmonthdate({input_x}):T", axis=alt.Axis(title="transaction date", titleFontSize=16, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            alt.Y(f"{input_y}:Q", axis=alt.Axis(title="percent change in revenue", titleFontSize=16, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+            color=alt.value('gold'),
+            tooltip=[alt.Tooltip(f"{input_y}:Q", title="pct_change_revenue", format=",f"), alt.Tooltip(f'yearmonthdate({input_x}):T', title="txn date")]
+        )
+        tt = linechart.mark_line(strokeWidth=30, opacity=0.01)
+        return linechart + tt
+
 def _format_arrow(val):
     return f"{'↑' if val > 0 else '↓'} {abs(val):.2f}%" if val != 0 and val != 999 else '-'
 
@@ -285,6 +311,10 @@ with col[1]:
    st.plotly_chart(choropleth, use_container_width=True)
    heatmap = make_heatmap(mv_txn_prod, 'year', 'month_name', 'location', 'revenue', selected_color_theme)
    st.altair_chart(heatmap, use_container_width=True)
+   linechart = make_linechart(line_pct_change)
+   st.markdown('### Percentage change in revenue by location')
+   if linechart:
+    st.altair_chart(linechart, use_container_width=True)
 
 with col[2]:
     st.markdown('#### Top location by category')
